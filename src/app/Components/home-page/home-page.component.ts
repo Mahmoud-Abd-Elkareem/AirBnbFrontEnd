7166edcd0d382838ede88d0761af2aa0da528e21
m4ri4am <mariam.algergawy@gmail>
2021-08-03 05:02:18 +0200

    logic searchbar(try)

--- src/app/Components/home-page/home-page.component.ts
+++ src/app/Components/home-page/home-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { Property } from 'src/app/Modules/property';
+import { HomeService } from 'src/app/Services/home.service';
 
 @Component({
   selector: 'app-home-page',
@@ -8,13 +13,28 @@ import { Component, OnInit } from '@angular/core';
   template:` <app-landing-page-navbar-component></app-landing-page-navbar-component>`
 })
 export class HomePageComponent implements OnInit {
+city:Property[]=[];
 
+  search(){
+    this.homeserve.GetByCity().subscribe(
+      a=>console.log(a),
+      e=>console.log(e)
+    )
+}
+// mystring:string | undefined;
 
+// onSubmit(mystring:string) {
+//   console.log(mystring);
+// }
+// fleixble(){
+//   this.homeserve.GetByCity()
+// }
 
-  constructor() { }
+  constructor(public homeserve:HomeService ,public ar:ActivatedRoute) { }
 
   ngOnInit(): void {
-
-
+  this.homeserve.GetByCity().subscribe(
+    a=>{this.city=a;console.log(a)}
+  )
   }
 }
---
+++ src/app/Modules/DTO/Dto.ts
@@ -0,0 +1,11 @@
+export class Dto {
+    public propertyId!:number;
+    public longitude!:string;
+    public latitude!:string;
+    public pricePerNight!:number;
+    public name!:string;public description!:string;
+    public maxOccupation!:string;public totalBeds!:number;
+    public totalRooms!:number;public totalBathrooms!:number;
+    public country!:string;public city!:string;public address!:string;public UniqueStays!:string;
+    public type!:string;public hostId!:number;
+}
